2156d3c6ad2973d5534cb5c35f0d85af23759309
Sven Huff <info@sven-huff.de>
2022-03-29 21:46:12 +0200

    Deleted search funtion. Started with contemps

--- refugee-master/script.js
+++ refugee-master/script.js
@@ -2,10 +2,6 @@ $(function () {
   $('[data-toggle="popover"]').popover();
 });
 
-meritObjects.forEach((object) => {
-  console.log(parseInt(object.birth, 10));
-});
-
 $(".popover-dismiss").popover({
   trigger: "focus",
 });
@@ -13,19 +9,6 @@ $(".popover-dismiss").popover({
 // === Variables related to global
 
 let refugeeMap = document.getElementById("refugee-map");
-
-// === Variables related to contemporary modal
-let closeContemp = document.getElementById("close--contemp");
-let contempModal = document.getElementsByClassName(
-  "modal-container--contemp"
-)[0];
-// let displayContemp = document.getElementById("allContemp");
-
-// ===== Window on click, wenn modals / markers offen, close.
-
-// the marker is a red dot which marks the refugee object
-
-let markerPlacement = document.getElementById("marker");
 
 // THE LINEAGE MASTER SEARCH MODULE
 // Initiate array to collect all names of merit objects in alphabetical order. To select them via VALUE of "searchLineageMaster"
@@ -44,36 +27,6 @@ alphabeticalMaster.forEach((e, i) => {
   let options = new Option(e, i);
   lineagMasterSearch.add(options, undefined);
 });
-
-// End of Placing all Lineage Masters into select field ////
-
-let placeMarker = (searchedMasterCoord) => {
-  let searchedMasterCoordArr = searchedMasterCoord.split(",");
-  // MarkerPlacement
-  const searchMasterCoordX = parseInt(searchedMasterCoordArr[0]);
-  const searchMasterCoordY = parseInt(searchedMasterCoordArr[1]);
-  markerPlacement.style.left = searchMasterCoordX - 15 + "px";
-  markerPlacement.style.top = searchMasterCoordY + 90 + "px";
-};
-
-// ====== Drop Down Menu: Search for object =====
-lineagMasterSearch.addEventListener("click", (e) => {
-  marker.style.visibility = "visible";
-  let index = lineagMasterSearch.value;
-  let searchedMasterName = alphabeticalMaster[index];
-
-  // Array with all Maters from DOM
-  let allMasters = document.querySelectorAll("area");
-
-  allMasters.forEach((e, i) => {
-    if (searchedMasterName == allMasters[i].alt) {
-      searchedMasterCoord = allMasters[i].coords;
-    }
-  });
-  placeMarker(searchedMasterCoord);
-});
-
-// END: LINEAGE MASTER SEARCH MODULE
 
 // Variables to place content inside the modal
 let objectName = document.getElementById("objectName");
@@ -140,45 +93,25 @@ renderModal = (clickedObject) => {
   document.getElementById("objectImg").src = imgPath;
 };
 
-// ===== Renders Modal for Contemporaries ======
-// =============================================
-
-let searchContemporary = () => {
-  let contemporaries = [];
-
-  meritObjects.forEach((e) => {
-    // All cases in which target is born AFTER contemporaries birth
-    if (
-      parseInt(clickedObject.birth) >= parseInt(e.birth) &&
-      parseInt(clickedObject.death) <= parseInt(e.death)
-    ) {
-      contemporaries.push(
-        e.firstName + " " + parseInt(e.birth) + "-" + parseInt(e.death)
-      );
-    }
-    // All cases in which target is born BEFORE contemporaries birth
-    else if (
-      parseInt(clickedObject.birth) <= parseInt(e.birth) &&
-      parseInt(clickedObject.death) >= parseInt(e.birth)
-    ) {
-      contemporaries.push(
-        e.firstName + " " + parseInt(e.birth) + "-" + parseInt(e.death)
-      );
-    }
-  });
-  renderModalContemp(contemporaries);
-};
-
-// const contempus = document.getElementById("contemporary");
-// contempus.addEventListener("click", searchContemporary());
-
 // ==== Close modal & modalContemp depending on click target
 
 areaClickHandler = (event) => {
   event.preventDefault();
   const area = event.target;
-  const chosenObject = area.title; //The name of element clicked on.
+  const chosenObject = area.title; //The name of element clicked on
 
+  // TODO: Hier per filter loopen und nach Zeitgenossen suchen
+  meritObjects.forEach((entry) => {
+    if (entry.firstName == chosenObject) {
+      console.log(entry.firstName);
+      // console.log(entry.birth);
+      meritObjects.filter((person) => {
+        if (person.birth < entry.birth) {
+          console.log(person.firstName);
+        }
+      });
+    }
+  });
   // This part retrieves the coords data from HTML area
   const elementCoords = area.coords;
 
